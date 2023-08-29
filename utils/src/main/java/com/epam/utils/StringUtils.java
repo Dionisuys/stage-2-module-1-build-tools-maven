0ea41d1ee04f4c166741ce372bfe0addcf98718a
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-08-29 18:34:21 +0300

    Module #1. Task #2. Build Tools. Maven solution

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,13 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+
+        try {
+            double value = Double.parseDouble(str);
+            return value > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
     }
 }
